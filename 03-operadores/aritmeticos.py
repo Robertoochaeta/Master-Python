95b2dac6bc220648d3a382dc329f94d7b0549333
Roberto <mario24roberto@gmail.com>
2022-01-03 22:03:18 -0600

    Aprendiendo operadores

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,14 @@
+#Operadores Aritmeticos
+numero1 = 80
+numero2 = 4 #Operador de asignación
+
+resta = numero1 - numero2
+multiplicacion= numero1*numero2
+division= numero1 / numero2
+resto = numero1%numero2
+print("*******************Calculadora****************")
+print(f"La Suma es: {numero1+numero2}")
+print(f"La resta es: {resta}")
+print(f"La multiplicacion es: {multiplicacion}")
+print("La division es:", division)
+print("EL resto de la división es: " , resto)
---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,27 @@
+#Operadores de Asignación
+edad = 55
+print(edad)
+
+edad +=5
+print(edad)
+
+edad -=5
+print(edad)
+
+#Incremento o decremento
+year = 2021
+year = year + 1
+print(year)
+year = year -1
+year +=1
+
+print(year)
+year -=1
+print(year)
+
+#Pre Incremento
+year= 1 + year
+print(year)
+#Pre Decremento
+year = 1 - year
+print(year)
