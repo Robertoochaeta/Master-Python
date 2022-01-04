c4c8768e732bbe09f5d6dd6c655e13d2a58c88ca
Roberto <mario24roberto@gmail.com>
2022-01-03 22:03:52 -0600

    Agregando condiciones a los colores

---
+++ 04-entradas/entradas.py
@@ -0,0 +1,5 @@
+#entrada
+nombre = input("¿Cual es tu nombre: ")
+edad = input("Cual es tu edad?: ")
+#Salida
+print(f"Me Alegro de conocerte bienvenido {nombre}, veo que tienes la edad de {edad} años")
---
+++ 05-condicionales/main.py
@@ -0,0 +1,8 @@
+#Ejemplo1#"######
+
+#color = "verde"
+color= input("Adivina el color favorito: ")
+if color== "rojo":
+    print("El color es rojo")
+else:
+    print("El color es incorrecto")
