d89d8f19fca8bc3c11e2e4fd3592dde92c5c4385
Roberto <mario24roberto@gmail.com>
2022-01-04 10:31:53 -0600

    Agregando el operador mayor o igual que

--- 05-condicionales/main.py
+++ 05-condicionales/main.py
@@ -6,3 +6,13 @@ if color== "rojo":
     print("El color es rojo")
 else:
     print("El color es incorrecto")
+
+#Operadores de comparacion
+"""
+Igual que 
++"""
+year =2020
+if year >= 2022:
+    print("Estamos de 2022 en adelante")
+else:
+    print("Es un año anterior a 2021")
