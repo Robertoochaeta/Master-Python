4dbcc6a32120d873f3cf4aa5df71b836f553e901
Roberto <mario24roberto@gmail.com>
2022-01-26 18:55:38 -0600

    ejecutando if anidados

--- 05-condicionales/main.py
+++ 05-condicionales/main.py
@@ -9,10 +9,26 @@ else:
 print("\n####################################################################")
 #Operadores de comparacion
 
-year = int(input("¿En que año estamos?"))
-if year < 2022:
-    print("Estamos antes de 2022")
-else:
-    print("Es un año inferior a 2022")
+#year = int(input("¿En que año estamos?"))
+#if year < 2022:
+ #   print("Estamos antes de 2022")
+#else:
+  #  print("Es un año inferior a 2022")
 
-#Ejemplo 3 
+#Ejemplo 3
+print("\n####################################################################")
+nombre = 'Roberto'
+ciudad = 'San Francisco'
+continente= 'Oceania'
+edad = 18
+mayoriaEdad=18
+
+if edad >= mayoriaEdad:
+    print(f"{nombre} Es mayor de edad")
+    if continente != "America":
+        print("El Usuario no es americano")
+    else:
+        print(f" es Americano y la ciudad es {ciudad}")
+
+else:
+    print(f" {nombre} No es Mayor de edad")
