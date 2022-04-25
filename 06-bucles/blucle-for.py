d16c4cd7b09af5bdc641a57ab449ffa90e355bc1
Roberto <mario24roberto@gmail.com>
2022-04-24 21:30:59 -0600

    Sumando los valores con cada iteracion

--- 06-bucles/blucle-for.py
+++ 06-bucles/blucle-for.py
@@ -4,6 +4,9 @@ for variable in elemento iterable (lista, rango)
 """
 
 contador = 0
-
+resultado = 0
 for contador in range(0,10):
     print("Voy por el :" + str(contador))
+    resultado +=contador
+
+print(f"El resultado es: {resultado}" )
